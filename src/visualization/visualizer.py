b49ad7ebd00f513b28d687f4aba74f30c1b890ae
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2025-05-01 13:17:07 +0300

    [add] grid visualizer

---
+++ src/visualization/visualizer.py
@@ -0,0 +1,31 @@
+from abc import ABC
+
+import matplotlib
+import matplotlib.image as mpimg
+
+
+class Visualizer(ABC):
+    @staticmethod
+    def _render_image(
+        image_path: str,
+        ax: matplotlib.axes.Axes,
+    ) -> None:
+        """Load an image and display it in the given axis grid position."""
+        if image_path:
+            img = mpimg.imread(image_path)
+            ax.imshow(img)    
+
+    def _render_cell(
+        self,
+        image_path: str,
+        ax: matplotlib.axes.Axes,
+        axis: bool = False,
+    ):
+        if image_path:
+            self._render_image(image_path=image_path, ax=ax)
+
+        if not axis:
+            ax.axis("off")
+
+        ax.set_xticks([])
+        ax.set_yticks([])
---
+++ src/visualization/visualizer_grid.py
@@ -0,0 +1,57 @@
+from matplotlib import pyplot as plt
+
+from src.dataclass.grid import Grid
+from src.repository.repository import Repository
+from src.visualization.visualizer import Visualizer
+
+OFFSET = 0.15
+
+
+class VisualizerGrid(Visualizer):
+    def draw(
+        self,
+        grid: Grid,
+        repository: Repository,
+        title: str = None,
+        show_borders: bool = False,
+        save_path: str | None = None,
+        show: bool = False,
+    ):
+        nrows, ncols = grid.height, grid.width
+        fig, ax = plt.subplots(
+            nrows, ncols,
+            figsize=(ncols, nrows),
+        )
+
+        if title:
+            fig.suptitle(title)
+
+        for x, y in grid.cells():
+            cell_ax = ax[x, y]
+
+            image = repository.patterns[0].asset
+
+            self._render_cell(
+                image_path=image,
+                ax=cell_ax,
+                axis=show_borders,
+            )
+
+        plt.subplots_adjust(
+            left=OFFSET,
+            right=1 - OFFSET,
+            bottom=OFFSET,
+            top=1-OFFSET,
+            hspace=0,
+            wspace=0,
+        )
+
+        if save_path:
+            plt.savefig(save_path, bbox_inches="tight", pad_inches=0)
+        if show:
+            plt.show()
+
+        plt.close()
+
+
+vis_grid = VisualizerGrid()
